6870a1cad9c4680d12e9e265d3756822c6306a16
Jose Maria Saborido <josemaria.saborido@iesdonana.org>
2019-10-30 12:47:24 +0100

    Completo el script 5 del primer boletin del tema 4

--- Actividades/Actividades Tema 4/BOLETÍN 1. FUNCIONES Y ARRAYS/5/script.js
+++ Actividades/Actividades Tema 4/BOLETÍN 1. FUNCIONES Y ARRAYS/5/script.js
@@ -1,14 +1,21 @@
 /**5. Realiza los cambios necesarios en el ejercicio anterior para que la última acción de generar
 un nuevo vector a partir de los valores de otro, dejando fuera el primer y último, pase a ser
 un método del objeto Array.*/
+
+Array.prototype.quitarLimites = function () {
+    let b;
+    b = this.slice();
+    b = b.sort();
+    b.splice(0, 1);
+    b.splice(b.length - 1, 1);
+    return b;
+};
 function vector() {
-    var a=[];
+    let a = [];
     for (let i = 0; i < 10; i++) {
-        a[i]=(Math.floor((Math.random()*1001)))
+        a[i] = (Math.floor((Math.random() * 1001)))
     }
-    var b=a.slice();
-    b=b.sort();
-    b.splice(0,1);
-    b.splice(b.length-1,1);
-    document.getElementById("enviar").innerHTML=`vector original: ${a}<br>vector con valores maximo y minimo eliminado: ${b}`
+    let b = a.quitarLimites()
+
+    document.getElementById("enviar").innerHTML = `vector original: ${a}<br>vector con valores maximo y minimo eliminado: ${b}`
 }
